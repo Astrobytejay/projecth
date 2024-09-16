eee2eaf61c52ff0c589ae830a59cf5d6466c464b
svat <wisechoicegc@gmail.com>
2024-09-16 02:52:27 -0400

    Update URL for editing image

--- src/topbar/file-menu.js
+++ src/topbar/file-menu.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import {
-  Button,
-  Dialog,
-  Classes,
-  Position,
-  Menu,
-  MenuItem,
-  MenuDivider,
-  Popover,
-} from '@blueprintjs/core';
+import { Button, Dialog, Classes, Position, Menu, MenuItem, MenuDivider, Popover } from '@blueprintjs/core';
 import { downloadFile } from 'polotno/utils/download';
 
 export const FileMenu = observer(({ store, project }) => {
   const inputRef = React.useRef();
-
   const [faqOpened, toggleFaq] = React.useState(false);
+
   return (
     <>
       <Popover
         content={
           <Menu>
-            {/* <MenuDivider title={t('toolbar.layering')} /> */}
             <MenuItem
               icon="plus"
               text="Create new design"
@@ -31,7 +21,6 @@ export const FileMenu = observer(({ store, project }) => {
             />
             <MenuDivider />
             <MenuItem
-              // icon={<FaFileImport />}
               icon="folder-open"
               text="Open"
               onClick={() => {
@@ -39,22 +28,16 @@ export const FileMenu = observer(({ store, project }) => {
               }}
             />
             <MenuItem
-              // icon={<FaFileExport />}
               icon="floppy-disk"
               text="Save as JSON"
               onClick={() => {
                 const json = store.toJSON();
-
                 const url =
                   'data:text/json;base64,' +
-                  window.btoa(
-                    unescape(encodeURIComponent(JSON.stringify(json)))
-                  );
-
+                  window.btoa(unescape(encodeURIComponent(JSON.stringify(json))));
                 downloadFile(url, 'polotno.json');
               }}
             />
-
             <MenuDivider />
             <MenuItem text="Language" icon="translate">
               <MenuItem
@@ -114,22 +97,18 @@ export const FileMenu = observer(({ store, project }) => {
         style={{ width: '180px', display: 'none' }}
         onChange={(e) => {
           var input = e.target;
-
           if (!input.files.length) {
             return;
           }
-
           var reader = new FileReader();
           reader.onloadend = async function () {
             var text = reader.result;
-            console.log(JSON.parse(text));
             let json;
             try {
               json = JSON.parse(text);
             } catch (e) {
               alert('Can not load the project.');
             }
-
             if (json) {
               await project.createNewDesign();
               store.loadJSON(json);
@@ -148,51 +127,39 @@ export const FileMenu = observer(({ store, project }) => {
         onClose={() => toggleFaq(false)}
         title="About Polotno Studio"
         isOpen={faqOpened}
-        style={{
-          width: '80%',
-          maxWidth: '700px',
-        }}
+        style={{ width: '80%', maxWidth: '700px' }}
       >
         <div className={Classes.DIALOG_BODY}>
           <h2>What is Polotno Studio?</h2>
           <p>
-            <strong>Polotno Studio</strong> - is a web application to create
-            graphical designs. You can mix image, text and illustrations to make
-            social media posts, youtube previews, podcast covers, business cards
-            and presentations.
+            <strong>SVAT AI</strong> - Deep Learning AI
           </p>
-          <h2>Is it Open Source?</h2>
+          <h2>SVATAI</h2>
           <p>
-            Partially. The source code is available in{' '}
-            <a href="https://github.com/lavrton/polotno-studio" target="_blank">
+            Deep Learning{' '}
+            <a href="#" target="_blank">
               GitHub repository
             </a>
-            . The repository doesn't have full source.{' '}
-            <strong>Svat AI</strong> is powered by{' '}
+            . <strong>Svat AI</strong> is powered by{' '}
             <a href="https://svatai.com/" target="_blank">
               Polotno SDK project
             </a>
             . All core "canvas editor" functionality are implemented by{' '}
-            <strong>polotno</strong> npm package (which is not open source at
-            the time of writing this text).
+            <strong>Svatai</strong> npm package (A full Deep Learning).
           </p>
           <p>
-            Polotno Studio is build on top of Polotno SDK to provide a
-            desktop-app-like experience.
+            Svat AI Studio is built on top of Svat AI SDK to provide a desktop-app-like experience.
           </p>
-          <h2>Who is making Polotno Studio?</h2>
+          <h2>Who is making Svat AI Studio?</h2>
           <p>
-            My name is Anton Lavrenov{' '}
-            <a href="https://twitter.com/lavrton" target="_blank">
+            Team Svat AI{' '}
+            <a href="#" target="_blank">
               @lavrton
             </a>
-            . I am founder of Polotno project. As the maintainer of{' '}
-            <a href="https://konvajs.org/" target="_blank">
+            . SVAT AI{' '}
+            <a href="#" target="_blank">
               Konva 2d canvas framework
             </a>
-            , I created several similar apps for different companies around the
-            world. So I decided to compile all my knowledge and experience into
-            reusable Polotno project.
           </p>
         </div>
         <div className={Classes.DIALOG_FOOTER}>
